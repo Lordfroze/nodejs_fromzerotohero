1b45366bcff0c8f4e6385b6a208c31d9397809ef
yoga <yoga@yogas-MacBook-Pro.local>
2026-07-04 21:33:28 +0700

    rest api bagian 2

--- users.js
+++ users.js
@@ -32,74 +32,97 @@ function parseBody(req) {
 // Membuat server HTTP
 const server = http.createServer(async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'OPTIONS')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+    // Handle jika terjadi OPTIONS request
+    if (req.method === 'OPTIONS') {
+        return res.end()
+    }
+
+    // parsing URL
+    const parsedUrl = url.parse(req.url, true)
+    const path = parsedUrl.pathname
+    const method = req.method
+
+    // get all users request
+    if (path === '/users' && method === 'GET') {
+        sendJSON(res, 200, users)
+    }
+
+    // get user by id request
+    else if (path.startsWith('/users/') && method === 'GET') {
+
+        const id = parseInt(path.split('/')[2]) // mengambil id 2 dari path untuk contoh
+        const user = users.find(u => u.id === id)
+        if (user) {
+            sendJSON(res, 200, user)
+        } else {
+            sendJSON(res, 404, { error: 'User tidak ditemukan' })
+        }
+    }
+
+    // create user request
+    else if (path === '/users' && method === 'POST') {
+        try {
+            const data = await parseBody(req)
+            if (!data.nama || !data.email) {
+                return sendJSON(res, 400, { error: 'Nama dan email wajib diisi' })
+            }
+
+            const newUser = {
+                id: nextId++,
+                ...data
+            }
+            users.push(newUser)
+            sendJSON(res, 201, newUser)
+        } catch (err) {
+            sendJSON(res, 400, { error: 'Body request tidak valid' })
+        }
+    }
+
+    // update user request
+    else if (path.startsWith('/users/') && method === 'PUT') {
+        const id = parseInt(path.split('/')[2])
+        const userIndex = users.findIndex(u => u.id === id)
+
+        if (userIndex === -1) {
+            return sendJSON(res, 404, { error: 'User tidak ditemukan' })
+        }
+
+        try {
+            const data = await parseBody(req)
+            if (!data.nama && !data.email) {
+                return sendJSON(res, 400, { error: 'Nama dan email wajib diisi' })
+            }
+
+            users[userIndex] = { id, ...data }
+            sendJSON(res, 200, users[userIndex])
+        } catch (err) {
+            sendJSON(res, 400, { error: 'Body request tidak valid' })
+        }
+    }
+
+    // delete user request
+    else if (path.startsWith('/users/') && method === 'DELETE') {
+        const id = parseInt(path.split('/')[2])
+        const initialLength = users.length
+
+        users = users.filter(u => u.id !== id) // menghapus user dengan id yang sesuai
+
+        if (users.length < initialLength) {
+            sendJSON(res, 200, { message: 'User berhasil dihapus' })
+        } else {
+            sendJSON(res, 404, { error: 'User tidak ditemukan' })
+        }
+    }
+
+    else {
+        sendJSON(res, 404, { error: 'Route tidak ditemukan' })
+    }
 })
 
-// Handle jika terjadi OPTIONS request
-if (req.method === 'OPTIONS') {
-    return res.end()
-}
-
-// parsing URL
-const parsedUrl = url.parse(req.url, true)
-const path = parsedUrl.pathname
-const method = req.method
-
-// get all users request
-if (path === '/users' && method === 'GET') {
-    sendJSON(res, 200, users)
-}
-
-// get user by id request
-else if (path.startsWith('/users') && method === 'GET') {
-
-    const id = parseInt(path.split('/')[2]) // mengambil id 2 dari path untuk contoh
-    const user = users.find(u => u.id === id)
-    if (user) {
-        sendJSON(res, 200, user)
-    } else {
-        sendJSON(res, 404, { error: 'User tidak ditemukan' })
-    }
-}
-
-// create user request
-else if (path === '/users' && method === 'POST') {
-    try {
-        const data = await parseBody(req)
-        if (!data.nama || !data.email) {
-            return sendJSON(res, 400, { error: 'Nama dan email wajib diisi' })
-        }
-
-        const newUser = {
-            id: nextId++,
-            ...data
-        }
-        users.push(newUser)
-        sendJSON(res, 201, newUser)
-    } catch (err) {
-        sendJSON(res, 400, { error: 'Body request tidak valid' })
-    }
-}
-
-// update user request
-else if (path.startsWith('/users') && method === 'PUT') {
-    const id = parseInt(path.split('/')[2])
-    const userIndex = users.findIndex(u => u.id === id)
-
-    if (userIndex === -1) {
-        return sendJSON(res, 404, { error: 'User tidak ditemukan' })
-    }
-
-    try {
-        const data = await parseBody(req)
-        if (!data.nama && !data.email) {
-            return sendJSON(res, 400, { error: 'Nama dan email wajib diisi' })
-        }
-
-        users[userIndex] = { id, ...data }
-        sendJSON(res, 200, users[userIndex])
-    } catch (err) {
-        sendJSON(res, 400, { error: 'Body request tidak valid' })
-    }
-}
+const PORT = 3000
+server.listen(PORT, () => {
+    console.log(`Server berjalan pada http://localhost:${PORT}`)
+})
